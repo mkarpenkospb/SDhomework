da480bb4481a76bf773a449590b76899a960734b
Maria Karpenko <mkarpenko.spb@mail.ru>
2020-03-04 04:00:00 +0300

    outer programm

--- src/main/java/itmo/karpenko/cli/Expression.java
+++ src/main/java/itmo/karpenko/cli/Expression.java
@@ -3,7 +3,12 @@ package itmo.karpenko.cli;
 
 //import sun.security.x509.AttributeNameEnumeration;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 
@@ -12,6 +17,9 @@ import java.util.*;
  */
 
 public class Expression {
+
+    public static boolean windows = System.getProperty("os.name").toLowerCase().contains("windows");
+
     public static Map<String, Programm> innerProgramms;
     static {
         innerProgramms = new HashMap<>();
@@ -67,15 +75,54 @@ public class Expression {
         return pipeLine;
     }
 
-    static void runProgram(String name, List<String> args) throws IOException {
-        if (innerProgramms.containsKey(name)) {
-            System.out.println(innerProgramms.get(name).execute(args));
+    static String[] getFullExec(Command cmd) {
+        StringBuilder res = new StringBuilder(cmd.name);
+        for (String args : cmd.args) {
+            res.append(" ").append(args);
+        }
+        return res.toString().split(" ");
+    }
+
+    static void runProgram(Command cmd) throws IOException, InterruptedException {
+        if (innerProgramms.containsKey(cmd.name)) {
+            System.out.println(innerProgramms.get(cmd.name).execute(cmd.args));
+        } else {
+            ProcessBuilder pBuild = new ProcessBuilder(windows ? "where" : "which", cmd.name);
+            Path pathToTarget = null;
+            try {
+                Process process = pBuild.start();
+                int errCode = process.waitFor();
+                if (errCode == 0) {
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(process.getInputStream()));
+                    pathToTarget = Paths.get(reader.readLine());
+                    System.out.println("Programm at "  + pathToTarget);
+//                    Runtime.getRuntime().exec(getFullExec(cmd));
+                    Process pr2 = new ProcessBuilder(getFullExec(cmd)).start();
+                    StringBuilder out = new StringBuilder();
+                    try (BufferedReader read = new BufferedReader(
+                            new InputStreamReader(pr2.getInputStream()))) {
+                        String line = null;
+                        while ((line = read.readLine()) != null) {
+                            out.append(line);
+                            out.append("\n");
+                        }
+                        System.out.println(out);
+                    }
+
+                } else {
+                    System.out.println("Programm not found");
+                }
+            } catch (IOException | InterruptedException except) {
+                System.out.println("Unexpected error");
+            }
+
         }
     }
 
-    static void execute(List<Command> commands) throws IOException {
+    static void execute(List<Command> commands) throws IOException, InterruptedException {
         if (commands.size() == 1) {
-            runProgram(commands.get(0).name, commands.get(0).args);
+            runProgram(commands.get(0));
         } else {
 //            for
             String nextArg = innerProgramms.get(commands.get(0).name).execute(commands.get(0).args);
--- src/main/java/itmo/karpenko/cli/Session.java
+++ src/main/java/itmo/karpenko/cli/Session.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Session {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         VariablesImpl environment = new VariablesImpl();
         Reader reader = new ReaderImpl();
         while (true) {
